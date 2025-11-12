71e20d059d457af6506831c38c5c105b295a4452
Sebastian-Inaba <Sebastian.inaba@gmail.com>
2025-11-12 14:20:32 +0900

    chore: add basename to correct routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/sw.js')
+            .register(`${import.meta.env.BASE_URL}sw.js`)
             .then((registration) =>
                 log(
                     'Service Worker registered with scope:',
